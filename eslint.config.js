5098e21b3f9896c0c3263732d3556edb67181a16
chidi esobe <chidiesobe@gmail.com>
2026-07-12 15:32:56 +0100

    chore: add ESLint/Prettier config and GitHub Actions CI

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/dist/**",
+      "**/.test-dist/**",
+      "**/node_modules/**",
+      "**/*.tsbuildinfo",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
