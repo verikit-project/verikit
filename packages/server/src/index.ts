0e832f36caa0c5f150e4ba9bd730e89da9c2f5e2
chidi esobe <chidiesobe@gmail.com>
2026-08-18 06:48:30 +0100

    feat(server): export OpenAPI document generator

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./adapter.js";
 export * from "./create-server.js";
+export * from "./openapi/index.js";
 export * from "./storage.js";
---
+++ packages/server/src/openapi/index.ts
@@ -0,0 +1,5 @@
+export * from "./error-schemas.js";
+export * from "./generate-openapi.js";
+export * from "./openapi-types.js";
+export * from "./resource-paths.js";
+export * from "./resource-schemas.js";
